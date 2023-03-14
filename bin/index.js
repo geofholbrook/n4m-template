e744a02c43fa1381d9a22c4ea4c5ed1ae760fb05
david <lostmorgan@gmail.com>
2023-03-14 19:06:47 -0400

    feat: add pkg json file to directory

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
-const fs = require('fs');
+const fs = require("fs");
 const dirName = process.argv[2];
 fs.mkdirSync(dirName);
+fs.writeFileSync(`${dirName}/package.json`, "");
 console.log(`Created ${dirName} directory`);
